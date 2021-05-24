7c63f91f467632da6953101c83ecaea975e51e1a
julien-tournier <80894875+julien-tournier@users.noreply.github.com>
2021-05-24 22:37:36 +0200

    V9

--- Nouvelle histoire.py
+++ Nouvelle histoire.py
@@ -9,10 +9,17 @@ def afficheZoneSaisie():
     zoneSaisie = entree.get()
     print('Votre mot de passe secret est : '+zoneSaisie)
     # return inutile : modification d'une variable globale
-
+def enreg():
+    "Enregistrer le fichier"
+    obfichier = open('fichier','a')
+    if entr1.get():
+        obfichier.write(entr1.get()+'\n')
+    obfichier.close()
 
 # Création de la fenêtre
 jeu = Tk()
+jeu.winfo_screenwidth()
+jeu.winfo_screenheight()
 jeu.geometry("900x600")
 
 
@@ -38,16 +45,23 @@ n.add(o2, text='Chapitre 2')  # Nom de l'onglet 2
 #Les Frames
     #Frame o1
 etiquette = Label(o1, text='Votre histoire :')
-etiquette.pack(padx=5, pady=5)
-etiquette.place(x=0, y=10)
+etiquette.pack(padx=5, pady=5, side=TOP)
+etiquette.place(x=5, y=00)
 
+scrollbar = Scrollbar(o1)
+texte_histoire = Text(o1, font=("Arial", 10), width=50, yscrollcommand=scrollbar.set)
+scrollbar.config(command=texte_histoire.yview)
 
-entree = Entry(o1, font=("Arial", 10),width=5000)
-entree.place(x=50, y=50, height=200, width=500000)
-entree.pack(padx=5, pady=5, side=BOTTOM)
+texte_histoire.place(x=0, y=0, height=50, width=50)
+texte_histoire.pack(padx=5, pady=20)
+scrollbar.pack(side=RIGHT, fill=Y)
+
 
 #Les boutons
+    #Boutons onglet 1
+Button(o1, text="Enregistrer",).pack()
 Button(o1, text='Quitter', command=jeu.destroy).pack()
+    #Boutons onglet 2
 Button(o2, text='En attente', command=None).pack()
 
 jeu.mainloop()
--- test.py
+++ test.py
@@ -1,12 +1,20 @@
-import tkinter as tk
+from tkinter import *
+def enreg():
+    "Enregistrer le fichier"
+    obfichier = open('fichier','a')
+    if entr1.get():
+        obfichier.write(entr1.get()+'\n')
+    obfichier.close()
 
-app = tk.Tk()
-app.geometry("400x200")
-
-entryExample = tk.Entry(app)
-entryExample.place(x = 10,
-        y = 10,
-        width=200,
-        height=100)
-
-app.mainloop()
+fen1 = Tk()
+fen1.title("Gestion des membres du club")
+tex1 = Label(fen1, text = 'Nom')
+entr1 = Entry(fen1)
+tex1.grid(row = 0, column = 0)
+entr1.grid(row = 0, column = 1)
+bou1 = Button(fen1, text = 'Quitter', command = fen1.quit)
+bou1.grid(row = 1, column = 1)
+bou2 = Button(fen1, text = 'Enregistrer', command = enreg)
+bou2.grid(row = 1, column = 0)
+fen1.mainloop()
+fen1.destroy()
